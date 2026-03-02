87898d5480292f7c17ac0b80f9a4c8fc88a9c8a4
Parthrajput-git <parthrajput3834@gmail.com>
2026-03-02 14:18:00 +0530

    All Coding questions

---
+++ Class1/script.js
@@ -0,0 +1,40 @@
+//1. Min / Max of Two 
+function number() {
+    let a = 10, b = 20, c = 5;
+    if (a > b) {
+
+        console.log("Max", a);
+        console.log("Min", b);
+
+    } else {
+        console.log("Max", b);
+        console.log("Min", a);
+
+    }
+
+}
+// number();
+
+// 2. Min / Max of Two / Three Numbers 
+function number1() {
+     let a = 10, b = 20, c = 5;
+// Min;
+let min=a;
+if(b<min)min=b;
+if(c<min)min=c;
+// Max
+let max=a;
+if(b>max)max=b;
+if(c>max)max=c;
+
+console.log("Minimum number  :",min);
+console.log("Maximum number  :",max);
+}
+
+// number1();
+
+// 3.Second largest number
+
+
+
+
